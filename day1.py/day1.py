31635e7a487785f3565b3f2648e4a252e2d72fa0
Aashish Kumawat <akkumawat3736@gmail.com>
2026-07-16 09:45:35 +0530

    day 1 progress

---
+++ day1.py/day1.py
@@ -0,0 +1,28 @@
+name = "Laxmi"
+age = 20
+city = "Jaipur"
+print(name)
+print(age)
+print(city)
+name = input("Enter your name: ")
+age = input("Enter your age: ")
+
+print("your name is", name)
+print("your age is", age)
+age = input("Enter your age: ")
+
+print(age)
+print(type(age))
+age = int(input("enter your age: "))
+print(age)
+print(type(age))
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print("hello", name)
+print("next year your age will be", age + 1)
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+city = input("you live in: ")
+print("Hello", name)
+print("you live in", city)
+print("After 5 years your age will be", age + 5)
