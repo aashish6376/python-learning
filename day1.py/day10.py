67ee903f5e4d9f48927888483efc25e0900eb854
Aashish Kumawat <akkumawat3736@gmail.com>
2026-07-23 22:44:38 +0530

    day 10 progress

---
+++ day1.py/day10.py
@@ -0,0 +1,30 @@
+marks = [85, 92, 78, 96, 88]
+print(marks[2])
+print(marks[0:3])
+marks.append(90)
+print(marks)
+marks.remove(78)
+print(marks)
+marks.sort()
+print(marks)
+marks.reverse()
+print(marks)
+marks.insert(2, 95)
+print(marks)
+marks[1] = 89
+print(marks)
+print(len(marks))
+for i in marks:
+    print(i)
+marks = [45, 34, 56, 57, 87]
+print(marks[2])
+marks.append('python')
+print(marks)
+marks = []
+for i in range(5):
+    num = int(input("Enter number: "))
+    marks.append(num)
+print(marks)
+marks.sort()
+print(marks)
+print(marks[4])
