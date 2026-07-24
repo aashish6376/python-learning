205b6baf06f9980472e0fa1b84bb53b7afc1eb88
Aashish Kumawat <akkumawat3736@gmail.com>
2026-07-24 23:02:53 +0530

    day11 progress

---
+++ day1.py/day11.py
@@ -0,0 +1,30 @@
+def greet():
+    print("Hello Aashish!")
+greet()
+def greet(name):
+    print("Hello", name)
+greet("Aashish")
+greet("Rahul")
+greet("Lokesh")
+def add(a, b):
+    return a + b
+answer = add(3, 4)
+print(answer)
+def welcome():
+    print("Welcome to python!")
+welcome()
+def greet(name):
+    print("Good morning", name)
+greet("Aashish")
+def multiply(a, b):
+    return a * b
+answer = multiply(3 , 4)
+print(answer)
+a = int(input("Enter number: "))
+if a%2 == 0:
+    print("True")
+else: print("False")
+def square(n):
+    return n * n
+print(square(4))
+print(square(7))
