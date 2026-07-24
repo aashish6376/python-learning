56687dd285de328c8cd86b42d73db4f37f913aa6
Aashish Kumawat <akkumawat3736@gmail.com>
2026-07-24 23:10:20 +0530

    day12 progress

---
+++ day1.py/day12.py
@@ -0,0 +1,11 @@
+word = "AASHISH"
+print(word[3])
+print(word[3:])
+print(word[:5])
+print(word[1:5])
+city = "JAIPUR"
+print(city[-1])
+print(city[-4:-1])
+print(city[-5:])
+print(city[:-2])
+print(len(city))
