15251c8b70ee4eb72746401255cc2dd21babe240
Aashish Kumawat <akkumawat3736@gmail.com>
2026-07-28 14:23:55 +0530

    day 16 progress

---
+++ day1.py/day16.py
@@ -0,0 +1,32 @@
+contacts = {
+    "Rahul": "9876543210",
+    "Lokesh" : "9807865649",
+    "Riya": "1234567890"
+}
+print(contacts["Lokesh"])
+car = {
+    "brand": "BMW",
+    "colour": "black",
+    "year": 2024
+}
+car["colour"] = "White"
+print(car["year"])
+print(car["colour"])
+student = {
+    "name": "Aashish"
+}
+student["Age"] = 22
+print(student)
+person = {
+    "name": "Riya",
+    "city": "Delhi",
+    "Age": 22
+}
+print(person["city"])
+person["city"] = "Jaipur"
+print(person["city"])
+print(person.keys())
+print(person.values())
+print(person.items())
+print(person.get("name"))
+print(person.get("phone"))
