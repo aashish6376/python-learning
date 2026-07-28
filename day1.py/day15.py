7a1c20239789a5f13baaea5ede6f04023e5e1bcc
Aashish Kumawat <akkumawat3736@gmail.com>
2026-07-28 13:50:49 +0530

    day15 progress

---
+++ day1.py/day15.py
@@ -0,0 +1,14 @@
+for i in range(1, 6):
+    if i == 4:
+        break
+    print(i)
+for i in range(1, 8):
+    if i == 3:
+        continue
+    print(i)
+for i in range(1,21,2):
+    print(i)
+for i in range(1,21):
+    if i%2 != 0:
+        continue
+    print(i)
